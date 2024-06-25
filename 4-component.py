3f028ea76d3a44590c90e3c2dda9c6b2cdb02b88
Kahaan Gandhi <kahaan@gmail.com>
2024-06-25 09:48:47 -0700

    fix 3.5 sigma threshold and walker initialization

--- 4-component.py
+++ 4-component.py
@@ -1,6 +1,5 @@
 import numpy as np
 import emcee
-import matplotlib.pylab as pl
 import matplotlib.pyplot as plt
 import multiprocessing
 import os
@@ -10,13 +9,44 @@ from numba import njit
 from tqdm import tqdm
 
 # calculates the rms noise in a given spectrum, should be robust to interloping lines, etc.
+def calc_noise_std(spectrum):
+    dummy_ints = np.copy(spectrum)
+    noise = np.copy(spectrum)
+    dummy_mean = np.nanmean(dummy_ints)
+    dummy_std = np.nanstd(dummy_ints)
+
+    # repeats 3 times to make sure to avoid any interloping lines
+    for chan in np.where(dummy_ints < (-dummy_std*1.))[0]:
+        noise[chan-10:chan+10] = np.nan
+    for chan in np.where(dummy_ints > (dummy_std*1.))[0]:
+        noise[chan-10:chan+10] = np.nan
+    noise_mean = np.nanmean(noise)
+    noise_std = np.nanstd(np.real(noise))
+
+    for chan in np.where(dummy_ints < (-noise_std*3.5))[0]:
+        noise[chan-10:chan+10] = np.nan
+    for chan in np.where(dummy_ints > (noise_std*3.5))[0]:
+        noise[chan-10:chan+10] = np.nan
+    noise_mean = np.nanmean(noise)
+    noise_std = np.nanstd(np.real(noise))
+
+    for chan in np.where(dummy_ints < (-dummy_std*3.5))[0]:
+        noise[chan-10:chan+10] = np.nan
+    for chan in np.where(dummy_ints > (dummy_std*3.5))[0]:
+        noise[chan-10:chan+10] = np.nan
+    noise_mean = np.nanmean(noise)
+    noise_std = np.nanstd(np.real(noise))
+
+    return noise_mean, noise_std
+
+# Calculates local RMS noise in a given spectrum by iteratively masking outliers. 3.5σ default, 6σ for weaker species. 
 def calc_noise_std(intensity, threshold=3.5):
     dummy_ints = np.copy(intensity)
     noise = np.copy(intensity)
     dummy_mean = np.nanmean(dummy_ints)
     dummy_std = np.nanstd(dummy_ints)
 
-    # repeats 3 times to make sure to avoid any interloping lines
+    # Repeat 3 times to make sure to avoid any interloping lines
     for chan in np.where(dummy_ints-dummy_mean < (-dummy_std*threshold))[0]:
         noise[chan-10:chan+10] = np.nan
     for chan in np.where(dummy_ints-dummy_mean > (dummy_std*threshold))[0]:
@@ -40,95 +70,87 @@ def calc_noise_std(intensity, threshold=3.5):
 
     return noise_mean, noise_std
 
-# reads in the data and returns the data which has coverage of a given species (from the simulated intensities)
-def read_file(filename, restfreqs, int_sim, oldformat=False, shift=0.0, GHz=False, plot=False, block_interlopers=True):
+
+# Reads in the data, returns the data which has coverage of a given species (from simulated intensities)
+def read_file(filename, restfreqs, int_sim, shift=0.0, GHz=False, plot=False, block_interlopers=True):
     data = np.load(filename, allow_pickle=True)
 
-    # Unpacking the data arrays
+    # Unpack data arrays
     freqs = data[0]
     intensity = data[1]
-
     if GHz:
         freqs *= 1000.
 
     relevant_freqs = np.zeros(freqs.shape)
     relevant_intensity = np.zeros(intensity.shape)
     relevant_yerrs = np.zeros(freqs.shape)
-
     covered_trans = []
 
+    # Iterate through rest frequencies to identify their corresponding spectral lines
     for i, rf in enumerate(restfreqs):
-        thresh = 0.05
-        if int_sim[i] > thresh*np.max(int_sim):
-            vel = (rf - freqs)/rf*300000 + shift
-            locs = np.where((vel<(.3+6.)) & (vel>(-.3+5.6)))
+        thresh = 0.05                                           # Set a threshold as 5% of the peak intensity...
+        if int_sim[i] > thresh*np.max(int_sim):                 # find significant simulated intensities...
+            vel = (rf - freqs)/rf*300000 + shift                # calculate velocity shift for each frequency...
+            locs = np.where((vel<(.3+6.)) & (vel>(-.3+5.6)))    # and filter for a velocity range
 
             if locs[0].size != 0:
                 noise_mean, noise_std = calc_noise_std(intensity[locs])
                 if block_interlopers and (np.max(intensity[locs]) > 6*noise_std):
-                    print("interloping line detected " + str(rf))
+                    print(f"{rf:10.4f} MHz  |  Interloping line detected.")
                     if plot:
                         plt.plot(freqs[locs], intensity[locs])
                         plt.show()
                 else:
+                    # Mark the transition and store relavent data
                     covered_trans.append(i)
-                    print("Found_line at: " + str(rf))
+                    print(f"{rf:10.4f} MHz  |  Line found. ")
                     relevant_freqs[locs] = freqs[locs]
                     relevant_intensity[locs] = intensity[locs]
-                    print("NOISE STD:")
-                    print(noise_std)
                     relevant_yerrs[locs] = np.sqrt(noise_std**2 + (intensity[locs]*0.1)**2)
-                    print("INTENSITY AT THIS LOCATION:")
-                    print(intensity[locs])
-                    print("ADDING THIS TO YERRS:")
-                    print(np.sqrt(noise_std**2 + (intensity[locs]*0.1)**2))
-
                 if plot:
                     plt.plot(freqs[locs], intensity[locs])
                     plt.show()
             else:
-                print("No data covering " + str(rf))
+                print(f"{rf:10.4f} MHz  |  No data. ")
     
+    # Filter out zero entries to return a sparse, small spectrum
     mask = relevant_freqs > 0
-
     relevant_freqs = relevant_freqs[mask]
     relevant_intensity = relevant_intensity[mask]
     relevant_yerrs = relevant_yerrs[mask]
-
+    
     return(relevant_freqs, relevant_intensity, relevant_yerrs, covered_trans)
 
 
-
-
+# Simulate molecular spectral emission lines for a set of observational parameters
 def predict_intensities(source_size, Ncol, Tex, dV, mol_cat):
     obs_params = ObsParams("test", source_size=source_size)
     sim = MolSim("mol sim", mol_cat, obs_params, [0.0], [Ncol], [dV], [Tex], ll=[7000], ul=[30000], gauss=False)
     freq_sim = sim.freq_sim
     int_sim = sim.int_sim
     tau_sim = sim.tau_sim
-
+    
     return freq_sim, int_sim, tau_sim
 
-
-
-# Apply a beam dilution correction factor
+# Apply a beam dilution correction factor to intensity data
 @njit(fastmath=True)
 def apply_beam(frequency, intensity, source_size, dish_size):
-    #create a wave to hold wavelengths, fill it to start w/ frequencies
+    # Convert frequencies to wavelenths (cm) 
     wavelength = cm/(frequency*1e6)
     
-    #fill it with beamsizes
-    beam_size = wavelength * 206265 * 1.22 / dish_size
+    # Compute beam size with diffraction-limited resolution formula, assuming each component is centered in the beam
+    beam_size = wavelength * 206265 * 1.22 / dish_size  # 206265 converts radians to arcseconds
     
-    #create an array to hold beam dilution factors
+    # Compute beam dilution factor as the ratio of beam area to source area (overlapping circles)
     dilution_factor = source_size**2/(beam_size**2 + source_size**2)
     
+    # Apply the beam dilution factor to the intensities
     intensity_diluted = intensity*dilution_factor
     
     return intensity_diluted
 
 
-
+# Construct a composite model of molecular line emissions
 @njit(fastmath=True)
 def make_model(freqs1, freqs2, freqs3, freqs4, ints1, ints2, ints3, ints4, ss1, ss2, ss3, ss4, datagrid0, datagrid1, vlsr1, vlsr2, vlsr3, vlsr4, dV, Tex):
     curr_model = np.zeros(datagrid1.shape)
@@ -138,22 +160,28 @@ def make_model(freqs1, freqs2, freqs3, freqs4, ints1, ints2, ints3, ints4, ss1, 
     model4 = np.zeros(datagrid1.shape)
     nlines = freqs1.shape[0]
 
+    # Compute Gaussian profiles for each source and sum them
     for i in range(nlines):
-        vel_grid = (freqs1[i]-datagrid0)/freqs1[i]*ckm
+        vel_grid = (freqs1[i]-datagrid0)/freqs1[i]*ckm  # Convert frequency shifts to velocity space
         mask = np.abs(vel_grid-5.8) < dV*10
+        
+        # Gaussian profiles for the intensity at each frequency point
         model1[mask] += ints1[i]*np.exp(-0.5*((vel_grid[mask] - vlsr1)/(dV/2.355))**2.)
         model2[mask] += ints2[i]*np.exp(-0.5*((vel_grid[mask] - vlsr2)/(dV/2.355))**2.)
         model3[mask] += ints3[i]*np.exp(-0.5*((vel_grid[mask] - vlsr3)/(dV/2.355))**2.)
         model4[mask] += ints4[i]*np.exp(-0.5*((vel_grid[mask] - vlsr4)/(dV/2.355))**2.)
 
+    # Apply the Planck function for thermal radiation, adjusted for background cosmic temperature (2.7 K)
     J_T = (h*datagrid0*10**6/k)*(np.exp(((h*datagrid0*10**6)/(k*Tex))) -1)**-1
     J_Tbg = (h*datagrid0*10**6/k)*(np.exp(((h*datagrid0*10**6)/(k*2.7))) -1)**-1
-
+    
+    # Apply the beam dilution correction to each model
     model1 = apply_beam(datagrid0, (J_T - J_Tbg)*(1 - np.exp(-model1)), ss1, 100)
     model2 = apply_beam(datagrid0, (J_T - J_Tbg)*(1 - np.exp(-model2)), ss2, 100)
     model3 = apply_beam(datagrid0, (J_T - J_Tbg)*(1 - np.exp(-model3)), ss3, 100)
     model4 = apply_beam(datagrid0, (J_T - J_Tbg)*(1 - np.exp(-model4)), ss4, 100)
 
+    # Sum all the individual models to create the composite model
     curr_model = model1 + model2 + model3 + model4
 
     return curr_model
@@ -234,36 +262,40 @@ def lnprob(theta, datagrid, mol_cat, prior_stds, prior_means):
     return lp + lnlike(theta, datagrid, mol_cat)
 
 
-def fit_multi_gaussian(datafile, fit_folder, catalogue, nruns, mol_name, restart=False):
-    print(f"Fitting column densities for {mol_name}")
-    datagrid = np.load(datafile, allow_pickle=True)
+def fit_multi_gaussian(datafile, fit_folder, catalogue, nruns, mol_name, prior_path, restart=True):
+    print(f"Fitting column densities for {mol_name}. restart = {restart}.")
 
-    ndim, nwalkers = 14, 100
+    ndim, nwalkers = 14, 128
+    datagrid = np.load(datafile, allow_pickle=True)
     mol_cat = MolCat(mol_name, catalogue) 
 
-    # source_size1, source_size2, source_size3, source_size4, Ncol1, Ncol2, Ncol3, Ncol4, Tex, vlsr1, vlsr2, vlsr3, vlsr4, dV 
-    # initial = [99, 65, 265, 262, 1.98e12, 6.22e12, 2.92e12, 4.88e12, 6.1, 5.595, 5.764, 5.886, 6.017, 0.121] # BENZONITRILE
-    initial = [37, 25, 56, 22, 2.47e12, 11.19e12, 2.20e12, 5.64e12, 6.7, 5.624, 5.790, 5.910, 6.033, 0.117] # HC9N
-    # initial = [42.8, 24.3, 47.9, 21.5, 5.8e13, 9.5e13, 4.e13, 1.06e14, 7.7, 5.603, 5.745, 5.873, 6.024, 0.1568] # LOOMIS
+    # Define initial parameter values for the MCMC walkers
+    # initial = np.array([99, 65, 265, 262, 1.98e12, 6.22e12, 2.92e12, 4.88e12, 6.1, 5.595, 5.764, 5.886, 6.017, 0.121]) # BENZONITRILE
+    initial = np.array([42.8, 24.3, 47.9, 21.5, 5.8e13, 9.5e13, 4.e13, 1.06e14, 7.7, 5.603, 5.745, 5.873, 6.024, 0.1568]) # LOOMIS
+    # initial = np.array([37, 25, 56, 22, 2.47e12, 11.19e12, 2.20e12, 5.64e12, 6.7, 5.624, 5.790, 5.910, 6.033, 0.117])  # HC9N
 
-    prior_path = os.path.join(fit_folder, mol_name, "chain.npy")
     if restart:
-        print("RESTART SET AS TRUE, LOADING INITIALS FROM CHAIN (HC9N)")
-        if os.path.exists(prior_path):
-            psamples = np.load(prior_path)[:,-200:,:].reshape(-1, ndim).T
-            prior_stds = np.std(psamples, axis=1)
-            prior_means = np.mean(psamples, axis=1)
-            initial = np.percentile(psamples, 50, axis=1)
-        else:
-            raise FileNotFoundError(f"No prior chain file found for {mol_name}, unable to proceed.")
-        
+        print("Initializing tight ball of walkers around hardcoded values.")
+        perturbation = np.array([1.e-1, 1.e-1, 1.e-1, 1.e-1, 1.e10, 1.e10, 1.e10, 1.e10, 1.e-3, 1.e-3, 1.e-3, 1.e-3, 1.e-3, 1.e-3])
+        pos = [initial + perturbation * np.random.randn(ndim) for i in range(nwalkers)]
     else:
-        print(f"USING HARDCODED MCMC PRIORS FROM HC9N")
-        prior_means = initial
-        prior_stds = [2.5, 2, 6.5, 2, 0.30e12, 1.75e12, 0.265e12, 1.185e12, 0.1, 0.0015, 0.001, 0.0035, 0.002, 0.002]
+        print(f"Initializing tight ball of walkers using prior chain values of {mol_name}.")
+        chain_data = np.load(os.path.join(fit_folder, mol_name, "chain.npy"))[:,-200:,:].reshape(-1, ndim).T
+        initial = np.median(chain_data, axis=1)
+        perturbation = np.array([1.e-1, 1.e-1, 1.e-1, 1.e-1, 1.e10, 1.e10, 1.e10, 1.e10, 1.e-3, 1.e-3, 1.e-3, 1.e-3, 1.e-3, 1.e-3])
+        pos = [initial + perturbation * np.random.randn(ndim) for i in range(nwalkers)]
 
-    pos = [initial + np.array([1.e-1,1.e-1,1.e-1,1.e-1, 1.e10,1.e10,1.e10,1.e10, 1.e-3, 1.e-3,1.e-3,1.e-3,1.e-3, 1.e-3])*np.random.randn(ndim) for i in range(nwalkers)]
+    # Load and process prior samples
+    psamples = np.load(prior_path).T
+    psamples_flat = psamples.reshape(ndim, -1)
     
+    # Compute prior means and standard deviations
+    prior_means = np.percentile(psamples_flat, 50, axis=1)
+    lower_percentiles = np.percentile(psamples_flat, 16, axis=1)
+    upper_percentiles = np.percentile(psamples_flat, 84, axis=1)
+    prior_stds = (np.abs(lower_percentiles - prior_means) + np.abs(upper_percentiles - prior_means)) / 2
+
+
     def is_within_bounds(params):
         source_size1, source_size2, source_size3, source_size4, Ncol1, Ncol2, Ncol3, Ncol4, Tex, vlsr1, vlsr2, vlsr3, vlsr4, dV = params
         if not (0. < source_size1 < 200):
@@ -321,7 +353,7 @@ def fit_multi_gaussian(datafile, fit_folder, catalogue, nruns, mol_name, restart
     return
 
 def init_setup(fit_folder, cat_folder, data_path, mol_name, block_interlopers):
-    print(f"Running setup for: {mol_name}, block interlopers = {block_interlopers}")
+    print(f"Running setup for: {mol_name}, block interlopers = {block_interlopers}.")
     try:
         os.mkdir(os.path.join(fit_folder, mol_name))
     except FileExistsError:
@@ -329,7 +361,7 @@ def init_setup(fit_folder, cat_folder, data_path, mol_name, block_interlopers):
 
     catfile = os.path.join(cat_folder, f"{mol_name}.cat")
     if not os.path.exists(catfile):
-        raise FileNotFoundError(f"No catalog file found at {catfile}")
+        raise FileNotFoundError(f"No catalog file found at {catfile}.")
 
     mol_cat = MolCat(mol_name, catfile)
     obs_params = ObsParams("init", source_size=40)
@@ -337,48 +369,33 @@ def init_setup(fit_folder, cat_folder, data_path, mol_name, block_interlopers):
     freq_sim = np.array(sim.freq_sim)
     int_sim = np.array(sim.int_sim)
 
-    print("Reading in data")
+    print(f"Reading in data from {data_path}")
     freqs_gotham, ints_gotham, yerrs_gotham, covered_trans_gotham = read_file(data_path, freq_sim, int_sim, block_interlopers=block_interlopers, plot=False)
     covered_trans_gotham = np.array(covered_trans_gotham, dtype=int)
     datagrid = [freqs_gotham, ints_gotham, yerrs_gotham, covered_trans_gotham]
     datagrid = np.array(datagrid, dtype=object)
-    print("DATAGRID 0: FREQS:")
-    print(datagrid[0])
-    print("DATAGRID 1: INTS:")
-    print(datagrid[1])
-    print("DATAGRID 2: YERRS:")
-    print(datagrid[2])
-    print("DATAGRID 3: COVERED TRANS:")
-    print(datagrid[3])
     datafile_path = os.path.join(fit_folder, mol_name, "all_" + mol_name + "_lines_GOTHAM_freq_space.npy")
     print("Saving data to: " + datafile_path)
-
-    print("DATAGRID FULL:")
-    print(datagrid)
     for i, item in enumerate(datagrid):
-        print(f"Element {i}, Type: {type(item)}, Shape: {item.shape if isinstance(item, np.ndarray) else 'N/A'}")
+        print(f"Datagrid element {i}  |  Type: {type(item)}  |  Shape: {item.shape if isinstance(item, np.ndarray) else 'N/A'}")
 
-
-    # Directly save the list using allow_pickle=True
     np.save(datafile_path, datagrid, allow_pickle=True)
-
 
     return datafile_path, catfile
 
 if __name__ == "__main__":
-    # Assuming this script is in the root directory of the repo
-    base_dir = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     # Define the new relative paths
     input_dict = {
-        'mol_name': 'hc9n_hfs',
-        'fit_folder': os.path.join(base_dir, 'fit_results'),
-        'cat_folder': os.path.join(base_dir, 'GOTHAM-catalogs'),
-        'data_path': os.path.join(base_dir, 'GOTHAM-data', 'hc9n_hfs_chunks.npy'),
+        'mol_name': 'hc11n',
+        'fit_folder': os.path.join(BASE_DIR, 'fit_results'),
+        'cat_folder': os.path.join(BASE_DIR, 'GOTHAM-catalogs'),
+        'data_path': os.path.join(BASE_DIR, 'GOTHAM-data', 'hc11n_chunks.npy'),
         'block_interlopers': True,
         'nruns': 10000,
         'restart': False,
-        'prior_path': os.path.join(base_dir, 'fit_results', 'hc9n_hfs', 'chain.npy'),
+        'prior_path': os.path.join(BASE_DIR, 'fit_results', 'hc9n_hfs', 'chain.npy'),
     }
 
     datafile, catalogue = init_setup(
@@ -395,10 +412,10 @@ if __name__ == "__main__":
         catalogue=catalogue,
         nruns=input_dict['nruns'],
         mol_name=input_dict['mol_name'],
+        prior_path=input_dict['prior_path'],
         restart=input_dict['restart']
     )
     
-    # Define parameter labels based on model setup
     param_labels = [
         "Source Size 1", "Source Size 2", "Source Size 3", "Source Size 4",
         "Column Density 1", "Column Density 2", "Column Density 3", "Column Density 4",
@@ -407,10 +424,10 @@ if __name__ == "__main__":
         "Delta V"
     ]
 
-    # Ensure the chain file path matches where data was saved
-    chain_file_path = os.path.join(input_dict['fit_folder'], input_dict['mol_name'], "chain.npy")
-    if os.path.exists(chain_file_path):
-        plot_results(chain_file_path, param_labels)
+    # Verify that chain file path matches where data was saved
+    CHAIN_PATH = os.path.join(input_dict['fit_folder'], input_dict['mol_name'], "chain.npy")
+    if os.path.exists(CHAIN_PATH):
+        plot_results(CHAIN_PATH, param_labels)
     else:
-        print(f"Chain file not found at {chain_file_path}")
+        print(f"Chain file not found at {CHAIN_PATH}.")
         
--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
-h = 6.626*10**(-34) #Planck's constant in J*s
-k = 1.381*10**(-23) #Boltzmann's constant in J/K
-kcm = 0.69503476 #Boltzmann's constant in cm-1/K
-ckm = 2.998*10**5 #speed of light in km/s
-ccm = 2.998*10**10 #speed of light in cm/s
-cm = 2.998*10**8 #speed of light in m/s
+h = 6.626*10**(-34)     # Planck's constant in J*s
+k = 1.381*10**(-23)     # Boltzmann's constant in J/K
+kcm = 0.69503476        # Boltzmann's constant in cm-1/K
+ckm = 2.998*10**5       # Speed of light in km/s
+ccm = 2.998*10**10      # Speed of light in cm/s
+cm = 2.998*10**8        # Speed of light in m/s
